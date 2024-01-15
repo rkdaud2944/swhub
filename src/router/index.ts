85e5da79fe768f0709e8d5320d3ea629d4e0a8f3
pkm <80870989+rkdaud2944@users.noreply.github.com>
2024-01-15 17:32:16 +0900

    no message

---
+++ src/router/index.ts
@@ -0,0 +1,280 @@
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
+import { useAuthStore } from "@/stores/auth";
+import { useConfigStore } from "@/stores/config";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    redirect: "/dashboard",
+    component: () => import("@/layouts/default-layout/DefaultLayout.vue"),
+    meta: {
+      middleware: "auth",
+    },
+    children: [
+      {
+        path: "/dashboard",
+        name: "dashboard",
+        component: () => import("@/views/Dashboard.vue"),
+        meta: {
+          pageTitle: "Dashboard",
+          breadcrumbs: ["Dashboards"],
+        },
+      },
+      {
+        path: "/crafted/account",
+        name: "account",
+        component: () => import("@/views/crafted/account/Account.vue"),
+        meta: {
+          breadcrumbs: ["Crafted", "Account"],
+        },
+        children: [
+          {
+            path: "overview",
+            name: "account-overview",
+            component: () => import("@/views/crafted/account/Overview.vue"),
+            meta: {
+              pageTitle: "Overview",
+            },
+          },
+          {
+            path: "settings",
+            name: "account-settings",
+            component: () => import("@/views/crafted/account/Settings.vue"),
+            meta: {
+              pageTitle: "Settings",
+            },
+          },
+        ],
+      },
+      {
+        path: "/apps/customers/getting-started",
+        name: "apps-customers-getting-started",
+        component: () => import("@/views/apps/customers/GettingStarted.vue"),
+        meta: {
+          pageTitle: "Getting Started",
+          breadcrumbs: ["Apps", "Customers"],
+        },
+      },
+      {
+        path: "/apps/customers/customers-listing",
+        name: "apps-customers-listing",
+        component: () => import("@/views/apps/customers/CustomersListing.vue"),
+        meta: {
+          pageTitle: "Customers Listing",
+          breadcrumbs: ["Apps", "Customers"],
+        },
+      },
+      {
+        path: "/apps/customers/customer-details",
+        name: "apps-customers-details",
+        component: () => import("@/views/apps/customers/CustomerDetails.vue"),
+        meta: {
+          pageTitle: "Customers Details",
+          breadcrumbs: ["Apps", "Customers"],
+        },
+      },
+      {
+        path: "/apps/calendar",
+        name: "apps-calendar",
+        component: () => import("@/views/apps/Calendar.vue"),
+        meta: {
+          pageTitle: "Calendar",
+          breadcrumbs: ["Apps"],
+        },
+      },
+      {
+        path: "/apps/chat/private-chat",
+        name: "apps-private-chat",
+        component: () => import("@/views/apps/chat/Chat.vue"),
+        meta: {
+          pageTitle: "Private Chat",
+          breadcrumbs: ["Apps", "Chat"],
+        },
+      },
+      {
+        path: "/apps/chat/group-chat",
+        name: "apps-group-chat",
+        component: () => import("@/views/apps/chat/Chat.vue"),
+        meta: {
+          pageTitle: "Group Chat",
+          breadcrumbs: ["Apps", "Chat"],
+        },
+      },
+      {
+        path: "/apps/chat/drawer-chat",
+        name: "apps-drawer-chat",
+        component: () => import("@/views/apps/chat/DrawerChat.vue"),
+        meta: {
+          pageTitle: "Drawer Chat",
+          breadcrumbs: ["Apps", "Chat"],
+        },
+      },
+      {
+        path: "/crafted/widgets/lists",
+        name: "widgets-list",
+        component: () => import("@/views/crafted/widgets/Lists.vue"),
+        meta: {
+          pageTitle: "Lists",
+          breadcrumbs: ["Crafted", "Widgets"],
+        },
+      },
+      {
+        path: "/crafted/widgets/statistics",
+        name: "widgets-statistics",
+        component: () => import("@/views/crafted/widgets/Statistics.vue"),
+        meta: {
+          pageTitle: "Statistics",
+          breadcrumbs: ["Crafted", "Widgets"],
+        },
+      },
+      {
+        path: "/crafted/widgets/charts",
+        name: "widgets-charts",
+        component: () => import("@/views/crafted/widgets/Charts.vue"),
+        meta: {
+          pageTitle: "Charts",
+          breadcrumbs: ["Crafted", "Widgets"],
+        },
+      },
+      {
+        path: "/crafted/widgets/mixed",
+        name: "widgets-mixed",
+        component: () => import("@/views/crafted/widgets/Mixed.vue"),
+        meta: {
+          pageTitle: "Mixed",
+          breadcrumbs: ["Crafted", "Widgets"],
+        },
+      },
+      {
+        path: "/crafted/widgets/tables",
+        name: "widgets-tables",
+        component: () => import("@/views/crafted/widgets/Tables.vue"),
+        meta: {
+          pageTitle: "Tables",
+          breadcrumbs: ["Crafted", "Widgets"],
+        },
+      },
+      {
+        path: "/crafted/widgets/feeds",
+        name: "widgets-feeds",
+        component: () => import("@/views/crafted/widgets/Feeds.vue"),
+        meta: {
+          pageTitle: "Feeds",
+          breadcrumbs: ["Crafted", "Widgets"],
+        },
+      },
+    ],
+  },
+  {
+    path: "/",
+    component: () => import("@/layouts/AuthLayout.vue"),
+    children: [
+      {
+        path: "/sign-in",
+        name: "sign-in",
+        component: () =>
+          import("@/views/crafted/authentication/basic-flow/SignIn.vue"),
+        meta: {
+          pageTitle: "Sign In",
+        },
+      },
+      {
+        path: "/sign-up",
+        name: "sign-up",
+        component: () =>
+          import("@/views/crafted/authentication/basic-flow/SignUp.vue"),
+        meta: {
+          pageTitle: "Sign Up",
+        },
+      },
+      {
+        path: "/password-reset",
+        name: "password-reset",
+        component: () =>
+          import("@/views/crafted/authentication/basic-flow/PasswordReset.vue"),
+        meta: {
+          pageTitle: "Password reset",
+        },
+      },
+    ],
+  },
+  {
+    path: "/",
+    component: () => import("@/layouts/SystemLayout.vue"),
+    children: [
+      {
+        // the 404 route, when none of the above matches
+        path: "/404",
+        name: "404",
+        component: () => import("@/views/crafted/authentication/Error404.vue"),
+        meta: {
+          pageTitle: "Error 404",
+        },
+      },
+      {
+        path: "/500",
+        name: "500",
+        component: () => import("@/views/crafted/authentication/Error500.vue"),
+        meta: {
+          pageTitle: "Error 500",
+        },
+      },
+    ],
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/404",
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior(to) {
+    // If the route has a hash, scroll to the section with the specified ID; otherwise, scroll toc the top of the page.
+    if (to.hash) {
+      return {
+        el: to.hash,
+        top: 80,
+        behavior: "smooth",
+      };
+    } else {
+      return {
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      };
+    }
+  },
+});
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  const configStore = useConfigStore();
+
+  // current page view title
+  document.title = `${to.meta.pageTitle} - ${import.meta.env.VITE_APP_NAME}`;
+
+  // reset config to initial state
+  configStore.resetLayoutConfig();
+
+  // verify auth token before each page change
+  authStore.verifyAuth();
+
+  // before page access check if page requires authentication
+  if (to.meta.middleware == "auth") {
+    if (authStore.isAuthenticated) {
+      next();
+    } else {
+      next({ name: "sign-in" });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
